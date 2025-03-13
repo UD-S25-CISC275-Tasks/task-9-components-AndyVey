e7f6a22bf0ce91e95eff4d99fcd4e3c2448c4f09
Andy Vey <avey@udel.edu>
2025-03-13 12:56:12 -0400

    Finished Apps

--- src/components/TwoDice.tsx
+++ src/components/TwoDice.tsx
@@ -12,5 +12,45 @@ export function d6(): number {
 }
 
 export function TwoDice(): React.JSX.Element {
-    return <div>Two Dice</div>;
+    const [leftDie, setLeftDie] = useState<number>(() => {
+        let first = d6();
+        let second = d6();
+        while (first === second) {
+            second = d6();
+        }
+        return first;
+    });
+    const [rightDie, setRightDie] = useState<number>(() => {
+        let first = d6();
+        let second = d6();
+        while (first === second) {
+            second = d6();
+        }
+        return second;
+    });
+    function rollLeft() {
+        setLeftDie(d6());
+    }
+    function rollRight() {
+        setRightDie(d6());
+    }
+    const isWin = leftDie === rightDie && leftDie !== 1;
+    const isLose = leftDie === 1 && rightDie === 1;
+
+    return (
+        <div>
+            <p>
+                {" "}
+                Left Die: <span data-testid="left-die">{leftDie}</span>
+            </p>
+            <p>
+                {" "}
+                Right Die: <span data-testid="right-die">{rightDie}</span>
+            </p>
+            <Button onClick={rollLeft}>Roll Left</Button>
+            <Button onClick={rollRight}>Roll Right</Button>
+            {isLose && <p>Lose</p>}
+            {isWin && <p>Win</p>}
+        </div>
+    );
 }
